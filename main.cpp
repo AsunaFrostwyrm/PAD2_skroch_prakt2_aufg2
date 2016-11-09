37b3525205f3bbc529e1190754628e0d3dc21e41
Zerozar <maximilian.daumlechner@stud.h-da.de>
2016-11-09 02:54:08 +0100

    Kommentare hinzugefügt

--- main.cpp
+++ main.cpp
@@ -44,16 +44,18 @@ int main(int argc, char** argv) {
         cin >> select;
 
         switch (select) {
-            case 'e':
-                for (int i = 0; i < 10; ++i) {
+            case 'e': //Eingabe eines normalen Parkplatzes
+		//Schleife zur Ermittlung aller belegten Plaetze
+                for (int i = 0; i < 10; ++i) { 
                     if (stellplaetze.at(i) == 1) {
                         counter++;
                     } else if (stellplaetze.at(i) == 2) {
                         dauerparkercounter++;
                     }
-                }
+		//Bedingung zum Abbruch wenn alle Plaetze belegt sind
                 if (counter != 10) {
                     cout << "Folgende Parkplätze können noch belegt werden:" << endl;
+		    //Schleife zur Ausgabe aller nicht belegten Plaetze
                     for (int i = 0; i < 10; ++i) {
                         if (stellplaetze.at(i) == 0) {
                             cout << "Parkplatz Nummer " << i + 1 << endl;
@@ -61,28 +63,35 @@ int main(int argc, char** argv) {
                     }
                     cout << "Welchen Parkplatz möchte Sie belegen?" << endl;
                 } else {
+		    //Abbruchausfuehrung wenn alle Plaetze belegt sind
                     cout << "Alle Parkplätze sind belegt!" << endl;
                     counter = 0;
                     dauerparkercounter = 0;
                     break;
                 }
+		//Eingabe des gewuenschten Parkplatzes
                 cin >> eingabe;
                 eingabe = eingabe - 1;
-
+		//Ueberpruefung, ob ein verwertbarer Index eingegeben wurde
                 if (eingabe >= 0 && eingabe <= 10) {
+		    //Ueberpruefung, ob der gewaehlte Platz schon vergeben ist
                     if (stellplaetze.at(eingabe) == 0) {
                         stellplaetze.fillexplicit(stellplaetze, 1, eingabe);
                         cout << "Parkplatz Nummer " << eingabe + 1 << " wurde nun belegt!" << endl;
                     } else {
+			//Abbruchausgabe
                         cout << "Parkplatz Nummer " << eingabe + 1 << " ist bereits belegt!" << endl;
                     }
                 } else {
+		    //Abbruchausgabe
                     cout << "Sie müssen eine Nummer zwischen 0 und 10 wählen! Ihre Nummer: " << eingabe + 1 << endl;
                 }
+		//Zuruecksetzen der Counter
                 counter = 0;
                 dauerparkercounter = 0;
                 break;
-            case 'a':
+            case 'a': //Eingabe eines Dauerparkplatzes
+		//Schleife zur Ermittlung aller belegten Plaetze
                 for (int i = 0; i < 10; ++i) {
                     if (stellplaetze.at(i) == 1) {
                         counter++;
@@ -90,18 +99,24 @@ int main(int argc, char** argv) {
                         dauerparkercounter++;
                     }
                 }
+		//Bedingung zum Abbruch wenn alle Plaetze belegt sind
                 if (counter != 10) {
+		    //Bedingung zum Abbruch wenn bereits 3 Dauerparker belegt sind
                     if (dauerparkercounter != 3) {
                         cout << "Folgende Parkplätze können noch als Dauerparkplätze belegt werden:" << endl;
+			//Schleife zur Ausgabe aller freien Parkplaetze
                         for (int i = 0; i < 10; ++i) {
                             if (stellplaetze.at(i) == 0) {
                                 cout << "Parkplatz Nummer " << i + 1 << endl;
                             }
                         }
                         cout << "Welchen Parkplatz möchten Sie als Dauerparkplatz festlegen?" << endl;
+			//Eingabe des gewuenschten Parkplatzes
                         cin >> eingabe;
                         eingabe = eingabe - 1;
+			//Ueberpruefung, ob ein verwertbarer Index eingegeben wurde
                         if (eingabe >= 0 && eingabe <= 10) {
+			    //Ueberpruefung, ob der gewaehlte Platz schon vergeben ist
                             if (stellplaetze.at(eingabe) == 0) {
                                 cout << "Um diesen Parkplatz belegen zu können müssen Sie das Kennzeichen angeben (Format: XX-XX-XXXX)" << endl;
                                 cin >> kennzeichen;
@@ -109,36 +124,46 @@ int main(int argc, char** argv) {
                                 dauerparker.fillexplicit(dauerparker, kennzeichen, eingabe);
                                 cout << "Parkplatz Nummer " << eingabe + 1 << " wurde nun mit dem Kennzeichen " << kennzeichen << " belegt!" << endl;
                             } else {
+				//Abbruchausgabe
                                 cout << "Parkplatz Nummer " << eingabe + 1 << " ist bereits belegt!" << endl;
                             }
                         } else {
+			    //Abbruchausgabe
                             cout << "Sie müssen eine Nummer zwischen 0 und 10 wählen! Ihre Nummer: " << eingabe + 1 << endl;
                         }
                     } else {
+			//Abbruchausgabe
                         cout << "Es wurde bereits die maximale Anzahl an Dauerparkern erreicht! (3)" << endl;
+			//Zuruecksetzen der Counter
                         counter = 0;
                         dauerparkercounter = 0;
                         break;
                     }
                 } else {
+		    //Abbruchausgabe
                     cout << "Alle Parkplätze sind bereits belegt!" << endl;
+		    //Zuruecksetzen der Counter
                     counter = 0;
                     dauerparkercounter = 0;
                     kennzeichen = "";
                     break;
                 }
+		//Zuruecksetzen der Counter
                 counter = 0;
                 dauerparkercounter = 0;
                 kennzeichen = "";
 
                 break;
-            case 'l':
+            case 'l': //Ausgabe aller belegten Parkplaetze
+		//Hochzaehlen des Indexes
                 for (int i = 0; i < 10; ++i) {
                     if (stellplaetze.at(i) == 0) {
                     } else if (stellplaetze.at(i) == 1) {
+			//Ausgabe fuer normalen Stellplatz
                         cout << "Parkplatz Nummer " << i + 1 << " ist als normaler Parkplatz belegt!" << endl;
                         counter++;
                     } else if (stellplaetze.at(i) == 2) {
+			//Ausgabe fuer Dauerstellplatz
                         cout << "Parkplatz Nummer " << i + 1 << " ist als Dauerparkplatz mit dem Kennzeichen " << dauerparker.at(i) << " belegt!" << endl;
                         counter++;
                     }
@@ -146,78 +171,86 @@ int main(int argc, char** argv) {
                 if(counter == 0){
                     cout << "Es sind derzeit keine Parkplätze belegt!" << endl;
                 }
+		//Zuruecksetzen des Counters
                 counter = 0;
                 break;
-            case 'o':
+            case 'o': //Platz freigeben mithilfe des Indexes
+		//Zaehlen aller belegten Plaetze
                 for (int i = 0; i < 10; ++i) {
                     if (stellplaetze.at(i) == 1) {
                         counter++;
                     }
                 }
+		//Abbruchbedingung wenn keine Parkplaetze belegt sind
                 if (counter != 0) {
                     cout << "Welchen Parkplatz möchten Sie freigeben?" << endl;
                     for (int i = 0; i < 10; ++i) {
-                        if (stellplaetze.at(i) == 0) {
+                        if (stellplaetze.at(i) == 0) { //Ausgabe aller belegten Parkplaetze 
                         } else if (stellplaetze.at(i) == 1) {
                             cout << "Parkplatz Nummer " << i + 1 << ": Normaler Parker" << endl;
                         } else if (stellplaetze.at(i) == 2) {
                             cout << "Parkplatz Nummer " << i + 1 << ": Dauerparker\tKennzeichen:" << dauerparker.at(i) << endl;
                         }
                     }
+		    //Eingabe des gewuenschten Parkplatzes
                     cin >> eingabe;
                     eingabe = eingabe - 1;
+		    //Ueberpruefung, ob ein verwertbarer Index eingegeben wurde
                     if (eingabe >= 0 && eingabe <= 10) {
-                        if(stellplaetze.at(eingabe) == 2){
+                        if(stellplaetze.at(eingabe) == 2){ //Auswahlmoeglichkeit fuer Abbruch der Freigabe bei Dauerparkplatz
                             cout << "Parkplatz Nummer " << eingabe+1 << " ist als Dauerparkplatz angegeben,\nmöchten Sie den Parkplatz trotzdem freigeben? (yes/no)" << endl;
                             cin >> yesno;
-                            if(yesno == "yes"){
+                            if(yesno == "yes"){ //Freigabe des Parkplatzes
                                 stellplaetze.fillexplicit(stellplaetze, 0, eingabe);
                                 dauerparker.fillexplicit(dauerparker, kennzeichen, eingabe);
                                 cout << "Parkplatz Nummer " << eingabe+1 << " wurde nun freigegeben!" << endl;
-                            }else if(yesno == "no"){
+                            }else if(yesno == "no"){ //Abbruch der Freigabe des Parkplatzes
                                 cout << "Freigabe des Parkplatzes wurde abgebrochen!" << endl;
                                 counter = 0;
                                 break;
                             }else{
+				//Abbruch bei nicht korrekt eingegebener Auswahl
                                 cout << "Ihre Eingabe entsprach nicht den Optionen 'yes' oder 'no', die Freigabe des Parkplatzes wurde abgebrochen!" << endl;
                                 counter = 0;
                                 break;
                             }
-                        }else{
+                        }else{ //Freigabe des Parkplatzes
                             stellplaetze.fillexplicit(stellplaetze, 0, eingabe);
                             cout << "Parkplatz Nummer " << eingabe+1 << " wurde nun freigegeben!" << endl;
                         }
-                    } else {
+                    } else { //Abbruch bei nicht verwertbaren Index
                         cout << "Sie müssen eine Nummer zwischen 0 und 10 wählen! Ihre Nummer: " << eingabe + 1 << endl;
                     }
-                } else {
+                } else { //Abbruch wenn kein Parkplatz belegt ist
                     cout << "Es ist noch kein Parkplatz belegt!" << endl;
                 }
+		//Zuruecksetzen des Counters
                 counter = 0;
                 break;
-            case 'p':
+            case 'p': //Freigabe aller Parkplaetze
+		//Warnung fuer die Freigabe aller Parkplaetze
                 cout << "Möchten Sie wirklich alle Parkplätze freigeben?! Alle Dauerparker und normalen Parker werden gelöscht! (yes/no)" << endl;
                 cin >> yesno;
-                if(yesno == "yes"){
+                if(yesno == "yes"){ //Freigabe aller Parkplaetze
                     stellplaetze.fill(stellplaetze, 0);
                     dauerparker.fill(dauerparker, kennzeichen);
                     cout << "Alle Parkplätze wurden nun freigegeben!" << endl;
-                }else if(yesno == "no"){
+                }else if(yesno == "no"){ //Abbruch bei nicht gewuenschter Freigabe
                     cout << "Die Freigabe aller Parkplätze wurde abgebrochen!" << endl;
                     break;
-                }else{
+                }else{ //Abbruch bei nicht korrekt eingegebener Auswahl
                     cout << "Ihre Eingabe entsprach nicht den Optionen 'yes' oder 'no', die Freigabe aller Parkplätze wurde abgebrochen!" << endl;
                     break;
                 }
                     
                 break;
-            case 'x':
-                stellplaetze.~myArray();
-                dauerparker.~myArray();
+            case 'x': //Beenden
+                stellplaetze.~myArray(); //Destruktoraufruf
+                dauerparker.~myArray(); //Destruktoraufruf
                 break;
         }
 
-    } while (select != 'x');
+    } while (select != 'x'); //Beenden des Programms
 
     return 0;
 }
--- myarray.hpp
+++ myarray.hpp
@@ -42,19 +42,19 @@ private:
 };
 
 template <class T, int N>
-myArray<T,N>::myArray() : myArray{ T{} } {
+myArray<T,N>::myArray() : myArray{ T{} } { //Standardkonstruktor
 
 }
 template <class T, int N>
-myArray<T,N>::myArray(T t){
+myArray<T,N>::myArray(T t){ //Konstruktor
     for(size_t i{}; i<N; ++i) elem[i] = t;
 }
 
 template <class T, int N>
-myArray<T,N>::~myArray(){
+myArray<T,N>::~myArray(){ //Destruktor
 }
 template <class T, int N>
-myArray<T,N>& myArray<T,N>::operator=( const myArray<T,N>& r){
+myArray<T,N>& myArray<T,N>::operator=( const myArray<T,N>& r){ //Operatorueberladung
     for(size_t i{}; i<N; ++i) elem[i] = r.elem[i];
     return *this;
 }
@@ -65,47 +65,47 @@ myArray<T,N>::myArray(const myArray& r){
 }
 
 template <class T, int N>
-myArray<T,N>::myArray(std::initializer_list<T> in){
+myArray<T,N>::myArray(std::initializer_list<T> in){ 
     std::copy(in.begin(), in.end(), elem);
 }
 template <class T, int N>
-T& myArray<T,N>::operator[](int n) {
+T& myArray<T,N>::operator[](int n) { //Operatorueberladung mit Rückgabe
     return elem[n];
 }
 
 template <class T, int N>
-T& myArray<T,N>::at(int n) {
+T& myArray<T,N>::at(int n) { //Rueckgabe des gespeicherten Wertes
     if (n < 0 || N <= n) throw std::out_of_range{"bad index"};
     return elem[n];
 }
 
-template <class T, int N>
-T* myArray<T,N>::dataPtr(int n) {
+template <class T, int N> 
+T* myArray<T,N>::dataPtr(int n) { //Pointerrueckgabe auf den eingegebenen Index
     return &(elem[n]);
 }
 
 template <class T, int N>
-int myArray<T,N>::size() const {
+int myArray<T,N>::size() const { //Groesse des Arrays
     return N;
 }
 
-template <class T, int N>
-void myArray<T,N>::fill(myArray<T, N>& a, const T& val) {
+template <class T, int N> 
+void myArray<T,N>::fill(myArray<T, N>& a, const T& val) { //Fuellt das komplette Array mit einem Wert
     for (size_t i{}; i < N; ++i) a[i] = val;
 }
 
 template <class T, int N>
-void myArray<T,N>::fillexplicit(myArray<T,N>& c, const T& value, int stelle){
+void myArray<T,N>::fillexplicit(myArray<T,N>& c, const T& value, int stelle){ //Fuellt das Array an einem bestimmten Index
     c[stelle] = value;
 }
 
 template<class T, int N>
-void myArray<T,N>::print(myArray<T,N>& d, int stelle){
+void myArray<T,N>::print(myArray<T,N>& d, int stelle){ //Textausgabe des Wertes an einem bestimmten Index
     std::cout << d[stelle] << std::endl;
 }
 
 template <class T, int N>
-void myArray<T,N>::printAll(myArray<T, N>& b) const{
+void myArray<T,N>::printAll(myArray<T, N>& b) const{ //Textausgabe aller Werte im Array
     for(size_t i{}; i < N;++i){ std::cout << b[i] << std::endl;}
 }
 
